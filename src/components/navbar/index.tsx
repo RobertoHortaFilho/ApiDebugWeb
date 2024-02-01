d66559d8ff7c70ae453ad07c7a56b3b0b8c5324d
roberto horta <robertohortafilho@gmail.com>
2024-02-01 17:25:38 -0300

    fix: remove a tag

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Navbar() {
   return (
     <div>
       <ul>
-        <li><a href="/">Home</a></li>
+        <li><Link href="/">Home</Link></li>
       </ul>
     </div>
   )
